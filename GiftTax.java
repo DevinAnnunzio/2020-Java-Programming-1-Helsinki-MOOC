e5f7824f0dffc2a113c5a595953e832118a6815e
Devin Annunzio <49703175+DevinAnnunzio@users.noreply.github.com>
2020-05-21 22:12:03 +0900

    Part 1 Exercise 37

---
+++ GiftTax.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class GiftTax {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = Integer.valueOf(scan.nextLine());
+        int result = number * 2;
+        System.out.println(result);
+
+    }
+}
